4fa11d06845ee10e2ca1093afba54a8eec186bb2
Mẫn Thị Kiều Linh <133326904+meowcoderchill@users.noreply.github.com>
2026-06-27 01:29:11 +0700

    Update version to 1.1.5 in nguonc_plugin.js

--- plugins/nguonc_plugin.js
+++ plugins/nguonc_plugin.js
@@ -6,7 +6,7 @@ function getManifest() {
     return JSON.stringify({
         "id": "nguonc",
         "name": "Hanako Amiya - Subtitle",
-        "version": "1.1.2",
+        "version": "1.1.5",
         "baseUrl": "https://phim.nguonc.com",
         "iconUrl": "https://raw.githubusercontent.com/youngbi/repo/main/plugins/nguonC.png",
         "isEnabled": true,
