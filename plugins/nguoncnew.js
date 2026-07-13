ffa3a67952b707750951bea97b65e24ee5585cb0
Mẫn Thị Kiều Linh <133326904+meowcoderchill@users.noreply.github.com>
2026-07-13 09:31:15 +0700

    Update plugin name in getManifest function

--- plugins/nguoncnew.js
+++ plugins/nguoncnew.js
@@ -5,7 +5,7 @@
 function getManifest() {
     return JSON.stringify({
         "id": "nguoncnew",
-        "name": "Phim NguonC Xoá Quảng Cáo",
+        "name": "Amiya Plugins Embed - No Ads",
         "version": "1.26",
         "baseUrl": "https://phim.nguonc.com",
         "iconUrl": "https://raw.githubusercontent.com/youngbi/repo/main/plugins/nguonC.png",
